f7c73d8a2ef74f9edc95a4f9f9888fee319ee44f
NhonTran <nhonvn2509@gmail.com>
2024-08-16 04:10:17 +0700

    Setup GitHub Actions for deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cicdtest/', // Thay <REPO_NAME> bằng tên repository của bạn
   plugins: [react()],
 })
